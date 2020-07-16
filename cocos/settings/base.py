1efd093aead3248a9d8766de4bbcc4ddd8c8c35a
root <root@CocosA.I>
2020-07-16 19:59:14 +0000

    lenguaje

--- cocos/settings/base.py
+++ cocos/settings/base.py
@@ -82,7 +82,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'es-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
