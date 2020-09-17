606c08ecbd9a41a4998fe3c89f61656aaa642f78
Jorgezepmed <jorge.david.zepmed@gmail.com>
2020-09-17 12:51:15 -0500

    add Key all

--- cocos/wsgi.py
+++ cocos/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+SECRET_KEY = '_c67ins@90e+yr2a4l&elgr*zq2d)z62ms!6=999w7kcbdq7fu'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cocos.settings')
 
